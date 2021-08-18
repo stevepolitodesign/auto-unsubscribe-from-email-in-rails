4ddd4371100950e543fb87ce3fa5aa05ddbfae99
Steve Polito <stevepolito@hey.com>
2021-08-18 19:52:14 -0400

    Add pseudo code.

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -1,4 +1,5 @@
 class MarketingMailer < ApplicationMailer
+  # after_action :prevent_delivery_if_recipient_opted_out
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -12,4 +13,10 @@ class MarketingMailer < ApplicationMailer
 
     mail to: @user.email, subject: @subject
   end
+
+  private
+
+  # def prevent_delivery_if_recipient_opted_out
+  #   mail.perform_deliveries = @user.subscribed_to_mailer? self.class.to_s
+  # end
 end
