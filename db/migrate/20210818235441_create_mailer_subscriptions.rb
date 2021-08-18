122628a080b391bf921c98c8e4792c0ba90bc5e7
Steve Polito <stevepolito@hey.com>
2021-08-18 19:57:29 -0400

    Update migration.

--- db/migrate/20210818235441_create_mailer_subscriptions.rb
+++ db/migrate/20210818235441_create_mailer_subscriptions.rb
@@ -3,9 +3,11 @@ class CreateMailerSubscriptions < ActiveRecord::Migration[6.1]
     create_table :mailer_subscriptions do |t|
       t.references :user, null: false, foreign_key: true
       t.boolean :subscribed
-      t.string :mailer
+      t.string :mailer, null: false
 
       t.timestamps
     end
+
+    add_index :mailer_subscriptions, :user, :mailer 
   end
 end
