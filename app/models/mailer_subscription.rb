718e6215b8568f3a84f0fe4f526f51f09916542a
Steve Polito <stevepolito@hey.com>
2021-08-18 21:31:06 -0400

    Add additional validations.

--- app/models/mailer_subscription.rb
+++ app/models/mailer_subscription.rb
@@ -1,6 +1,10 @@
 class MailerSubscription < ApplicationRecord
   belongs_to :user
 
-  validates :user, uniqueness: { scope: :mailer }
+  MAILERS = %w(MarketingMailer)
+
   validates :subscribed, inclusion: [true, false], allow_nil: true
+  validates :mailer, presence: true
+  validates :mailer, inclusion: MAILERS
+  validates :user, uniqueness: { scope: :mailer }
 end
