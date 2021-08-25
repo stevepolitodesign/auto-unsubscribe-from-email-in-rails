79253568d6ea37d580e7c65f2b17878b253d8de9
Steve Polito <stevepolito@hey.com>
2021-08-25 05:55:30 -0400

    WIP: Allow user to update their settings.

--- app/controllers/mailer_subscriptions_controller.rb
+++ app/controllers/mailer_subscriptions_controller.rb
@@ -1,5 +1,6 @@
 class MailerSubscriptionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_mailer_subscription, only: :update
 
   def index
     @mailer_subscriptions = MailerSubscription::MAILERS.items.map do |item|
@@ -8,9 +9,29 @@ class MailerSubscriptionsController < ApplicationController
   end
 
   def create
+    @mailer_subscription = current_user.mailer_subscriptions.build(mailer_subscription_params)
+    if @mailer_subscription.save?
+      redirect_to mailer_subscriptions_path, notice: "Preferences updated."
+    else
+      redirect_to mailer_subscriptions_path, alter: "#{@mailer_subscription.errors.full_messages.to_sentence}"
+    end
   end
 
   def update
-    @mailer_subscription.toggle!(:subscribed)
+    if @mailer_subscription.toggle!(:subscribed)
+      redirect_to mailer_subscriptions_path, notice: "Preferences updated."
+    else
+      redirect_to mailer_subscriptions_path, alter: "#{@mailer_subscription.errors.full_messages.to_sentence}"
+    end
   end
+
+  private
+
+    def mailer_subscription_params
+      params.require(:mailer_subscription).permit(:mailer, :subscribed, :user_id)
+    end
+
+    def set_mailer_subscription
+      @mailer_subscription = MailerSubscription.find(params[:id])
+    end
 end
