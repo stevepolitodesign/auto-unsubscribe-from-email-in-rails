a3ef262517150bafc16655aac2000187ae4d2a65
Steve Polito <stevepolito@hey.com>
2021-08-25 05:33:39 -0400

    Add helper methods.

--- app/models/mailer_subscription.rb
+++ app/models/mailer_subscription.rb
@@ -15,4 +15,25 @@ class MailerSubscription < ApplicationRecord
   validates :mailer, presence: true
   validates :mailer, inclusion: MAILERS.items.map{ |item|  item[:class] }
   validates :user, uniqueness: { scope: :mailer }
+
+  def details
+    MailerSubscription::MAILERS.items.select {|item| item[:class] == mailer }
+  end
+  
+  def name
+    details[0][:name]
+  end
+
+  def description
+    details[0][:description]
+  end
+
+  def action
+    subscribed? ? "Unsubscribe from" : "Subscribe to"
+  end
+
+  def call_to_action
+    "#{action} #{name}"
+  end
+
 end
