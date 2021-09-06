285358591dc9ade364d9958df8f2e0946bbcfc7c
Steve Polito <stevepolito@hey.com>
2021-09-06 16:24:33 -0400

    Brittle test.

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_home_url
+    get root_path
     assert_response :success
   end
 end
